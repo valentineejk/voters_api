99a00abd216a8639abf1269c0a27e630706da6b4
methu-ship <mettusella@gmail.com>
2026-06-24 08:25:05 +0100

    add state, NIN and phone validation

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,6 +1,8 @@
 package helpers
 
 import (
+	"strings"
+
 	"github.com/google/uuid"
 )
 
@@ -10,4 +12,21 @@ func GenerateVoterID() string {
 		return uuid.NewString()
 	}
 	return id.String()
+}
+
+var validNigerianStates = map[string]bool{
+	"abia": true, "adamawa": true, "akwa ibom": true, "anambra": true,
+	"bauchi": true, "bayelsa": true, "benue": true, "borno": true,
+	"cross river": true, "delta": true, "ebonyi": true, "edo": true,
+	"ekiti": true, "enugu": true, "fct": true, "gombe": true,
+	"imo": true, "jigawa": true, "kaduna": true, "kano": true,
+	"katsina": true, "kebbi": true, "kogi": true, "kwara": true,
+	"lagos": true, "nasarawa": true, "niger": true, "ogun": true,
+	"ondo": true, "osun": true, "oyo": true, "plateau": true,
+	"rivers": true, "sokoto": true, "taraba": true, "yobe": true,
+	"zamfara": true,
+}
+
+func ValidateState(state string) bool {
+	return validNigerianStates[strings.ToLower(strings.TrimSpace(state))]
 }
